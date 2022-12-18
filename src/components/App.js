bd01b9433e595a823cc446f1a09f69255caf0a6c
Libertad Pozos <libertad.pozos@gmail.com>
2022-12-18 09:51:02 +0000

    fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import '../styles/App.css';
 
 function App() {
+  const [ data, setData ] = useState();
+  const [ loading, setLoading ]= useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      const request = fetch('http:///0.0.0.0:8000/v1/news');
+      const response = await request;
+      const parsed = await response.json();
+      setLoading(false);
+      console.log(parsed);
+      setData(parsed);
+    }
+    getData();
+  }, []);
+
   return (
+    <>
+    {loading ? <p>Loading</p>
+    : 
     <div>
       <h1>Liberty News</h1>
-    </div>
+      <ul>
+        {data.articles.map((article, index)=>{
+          return(<li key={index}>{article.author}</li>)
+        })}
+      </ul>
+    </div>}
+    </>
   );
 }
 
